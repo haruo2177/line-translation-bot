444210ae166496ecac11d5ea8884cbc995885331
haruo2177 <haruo.yaguchi.2177@gmail.com>
2024-12-23 23:20:17 +0900

    refactor: standardize quotation marks and improve error messages

--- src/handlers/messageHandler.js
+++ src/handlers/messageHandler.js
@@ -1,29 +1,29 @@
-const { Client } = require('@line/bot-sdk');
-const config = require('../config');
-const { translateToKorean } = require('../services/translator');
+const { Client } = require("@line/bot-sdk");
+const config = require("../config");
+const { translateToKorean } = require("../services/translator");
 
 const client = new Client(config.line);
 
 async function handleMessage(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
+  if (event.type !== "message" || event.message.type !== "text") {
     return;
   }
 
   try {
     const translatedText = await translateToKorean(event.message.text);
     await client.replyMessage(event.replyToken, {
-      type: 'text',
-      text: translatedText
+      type: "text",
+      text: translatedText,
     });
   } catch (error) {
-    console.error('Handler error:', error);
+    console.error("Handler error:", error);
     await client.replyMessage(event.replyToken, {
-      type: 'text',
-      text: '翻訳中にエラーが発生しました。'
+      type: "text",
+      text: "翻訳中にエラーが発生しました／번역 중 오류가 발생했습니다",
     });
   }
 }
 
 module.exports = {
-  handleMessage
+  handleMessage,
 };
